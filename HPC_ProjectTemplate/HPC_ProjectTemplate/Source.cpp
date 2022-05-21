7557962a6a23f3d95caf285c063cdc96132ce3a9
mohamed nour_eldien <mohamednour20191700@gmail.com>
2022-05-21 22:47:36 +0200

    Sequential Code
    Sequential Code

--- HPC_ProjectTemplate/HPC_ProjectTemplate/Source.cpp
+++ HPC_ProjectTemplate/HPC_ProjectTemplate/Source.cpp
@@ -80,65 +80,132 @@ void createImage(int* image, int width, int height, int index)
 }
 
 
+// function to swap elements
+void swap(int* a, int* b) {
+	int t = *a;
+	*a = *b;
+	*b = t;
+}
+
+// function to rearrange array (find the partition point)
+int partition(int array[], int low, int high) {
+
+	// select the rightmost element as pivot
+	int pivot = array[high];
+
+	// pointer for greater element
+	int i = (low - 1);
+
+	// traverse each element of the array
+	// compare them with the pivot
+	for (int j = low; j < high; j++) {
+		if (array[j] <= pivot) {
+
+			// if element smaller than pivot is found
+			// swap it with the greater element pointed by i
+			i++;
+
+			// swap element at i with element at j
+			swap(&array[i], &array[j]);
+		}
+	}
+
+	// swap pivot with the greater element at i
+	swap(&array[i + 1], &array[high]);
+
+	// return the partition point
+	return (i + 1);
+}
+
+void quickSort(int array[], int low, int high) {
+	if (low < high) {
+		int pi = partition(array, low, high);
+		quickSort(array, low, pi - 1);
+		quickSort(array, pi + 1, high);
+	}
+}
+
+
 int main()
 {
+	int ImageWidth = 4, ImageHeight = 4;
+	int start_s, stop_s, TotalTime = 0;
+	System::String^ imagePath;
+	std::string img;
+	img = "..//Data//Input//test.png";
+	imagePath = marshal_as<System::String^>(img);
+	int* imageData = inputImage(&ImageWidth, &ImageHeight, imagePath);
+	int filtersize;
 	MPI_Init(NULL, NULL);
 	int size;
 	MPI_Comm_size(MPI_COMM_WORLD, &size);
 	int rank;
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-	cout << rank << endl;
-
-	int ImageWidth = 4, ImageHeight = 4;
-	int* imageData = NULL;
-	int filterSize;
-	cin >> filterSize;
-	int start_s, stop_s, TotalTime = 0;
-	if (rank == 0) {
-		System::String^ imagePath;
-		std::string img;
-		img = "..//Data//Input//test.png";
-		imagePath = marshal_as<System::String^>(img);
-		imageData = inputImage(&ImageWidth, &ImageHeight, imagePath);
-
-		//write code here
-		cout << endl << imageData << endl;
-		cout << ImageHeight << endl << ImageWidth << endl;
-
+	if (rank == 0)
+	{
 		start_s = clock();
-		createImage(imageData, ImageWidth, ImageHeight, 11);
+		cout << "Enter the filter size: ";
+		cin >> filtersize;
+		int* filter = new int[filtersize * filtersize];
+
+		//set all the the cells in the filter with zero
+		memset(filter, 0, filtersize * filtersize);
+
+		int indexOfCenter = filtersize / 2;
+
+		int startRowFilter, StartColumnFilter;
+
+
+		cout << "start calculating\n";
+		for (int i = 0; i < ImageHeight; i++)
+		{
+			for (int j = 0; j < ImageWidth; j++)
+			{
+				startRowFilter = indexOfCenter;
+				StartColumnFilter = indexOfCenter + 1;
+				filter[indexOfCenter] = imageData[i * ImageWidth + j];
+				for (int k = (i * ImageWidth + j); k < filtersize && k < ImageHeight; k++)
+				{
+					for (int l = (i * ImageWidth + j) + 1; l < filtersize && k < ImageWidth; l++)
+					{
+						filter[startRowFilter * filtersize + StartColumnFilter] = imageData[k * filtersize + l];
+						StartColumnFilter++;
+					}
+					StartColumnFilter = 0;
+					startRowFilter++;
+				}
+
+				startRowFilter = indexOfCenter;
+				StartColumnFilter = indexOfCenter - 1;
+
+				for (int k = (i * ImageWidth + j); k > filtersize && k > ImageHeight; k--)
+				{
+					for (int l = (i * ImageWidth + j) - 1; l > filtersize && k > ImageWidth; l--)
+					{
+						filter[startRowFilter * filtersize + StartColumnFilter] = imageData[k * filtersize + l];
+						StartColumnFilter--;
+					}
+					StartColumnFilter = 0;
+					startRowFilter--;
+				}
+				quickSort(filter, 0, (filtersize * filtersize));
+				imageData[i * ImageWidth + j] = filter[indexOfCenter];
+				memset(filter, 0, filtersize * filtersize);
+
+			}
+		}
+		createImage(imageData, ImageWidth, ImageHeight, 0);
 		stop_s = clock();
 		TotalTime += (stop_s - start_s) / double(CLOCKS_PER_SEC) * 1000;
 		cout << "time: " << TotalTime << endl;
-	}
-	MPI_Bcast(&ImageHeight, 1, MPI_INT, 0, MPI_COMM_WORLD);
-	MPI_Bcast(&ImageWidth, 1, MPI_INT, 0, MPI_COMM_WORLD);
-	int numOfBlocks = ImageHeight / size, extraRowsForBoundanarySubArray = int(filterSize/2);
-	int extraRowsForMiddleSubArrays = filterSize - 1;
-	MPI_Datatype boundarySubArray;
-	// height is the row, width is the column
-	int imageSize[2] = {ImageHeight,ImageWidth};
-	int boundarySubsize[2] = {numOfBlocks+extraRowsForBoundanarySubArray,ImageWidth};
-	int boundaryStart[2];
-	boundaryStart[1] = 0;
-	if (rank == 0) {
-		boundaryStart[0] = 0;
-	}
-	else if (rank == size - 1) {
-		boundaryStart[0] = numOfBlocks * rank - extraRowsForBoundanarySubArray;
-	}
-	PMPI_Type_create_subarray(2, imageSize, boundarySubsize, boundaryStart, MPI_ORDER_C,MPI_INT, & boundarySubArray);
-	MPI_Type_commit(&boundarySubArray);
-
-	MPI_Datatype middleSubArray;
-	
-	if (rank == 0) {
 		free(imageData);
+		return 0;
 	}
 	MPI_Finalize();
-	return 0;
+
 
 }
+
 
 
 
